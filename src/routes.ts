7d4476647c3544e04360f915feb343220a77d13c
JoaoOtavioSegantini <joaotavioos@outlook.com>
2021-02-27 09:19:01 -0300

    New route added

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { SurveysController } from "./controllers/SurveyController";
 import {UserController} from "./controllers/UserController"
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveysController();
 
-router.post("/users", userController.create)
+router.post("/users", userController.create);
+router.post("/surveys", surveyController.create);
 
 
 export { router }
