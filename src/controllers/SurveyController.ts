35941199f7b801c5da4a825554ba65bc61840356
JoaoOtavioSegantini <joaotavioos@outlook.com>
2021-02-27 09:16:59 -0300

    Survey controller configuration

--- src/controllers/SurveyController.ts
+++ src/controllers/SurveyController.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/SurveysRepository";
 
 class SurveysController {
-    async create(request: Request, response: Response) {}
+    async create(request: Request, response: Response) {
+        const { title, description} = request.body;
+        const surveyRepository = getCustomRepository(SurveysRepository);
+        const survey = surveyRepository.create({
+            title, description
+        })
+        await surveyRepository.save(survey);
+        return response.status(201).json(survey);
+    }
 }
 
 export { SurveysController }
