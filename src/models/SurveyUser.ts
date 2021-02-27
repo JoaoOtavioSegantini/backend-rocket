dbf54dbd57a33ea0f3662e5eb4809c3b91ed7302
JoaoOtavioSegantini <joaotavioos@outlook.com>
2021-02-27 16:42:57 -0300

    Create new model - SurveyUser

---
+++ src/models/SurveyUser.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("surveys_users")
+class SurveyUser {
+
+    @PrimaryColumn()
+    readonly id: string;
+    @Column()
+    value: number;
+    @Column()
+    user_id: string;
+    @Column()
+    survey_id: string;
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if(!this.id){
+            this.id = uuid();
+        }
+    }
+
+}
+
+export { SurveyUser };
