e193dbe9e6bab5a8508981dd32a894ec21320c00
JoaoOtavioSegantini <joaotavioos@outlook.com>
2021-02-27 15:23:14 -0300

    Add test base

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import "./database"
-import { router } from "./routes";
 import "reflect-metadata";
+import "./database";
+import createConnection from "./database";
+import { router } from "./routes";
 
+createConnection();
 const app = express();
 
 app.use(express.json())
 app.use(router);
 
 
-export {app};
+export { app };
+
