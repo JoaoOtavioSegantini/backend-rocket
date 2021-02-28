ca819ce793fb3f24d94aeb4dd5217201d54e6260
JoaoOtavioSegantini <joaotavioos@outlook.com>
2021-02-28 09:47:59 -0300

    Add survey response functionality

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AswerController } from "./controllers/AswerController";
 import { SendEmailController } from "./controllers/SendEmailController";
 import { SurveysController } from "./controllers/SurveyController";
 import {UserController} from "./controllers/UserController"
@@ -8,8 +9,10 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveysController();
 const sendEmailController = new SendEmailController();
+const aswerController = new AswerController();
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
 router.post("/sendEmail", sendEmailController.execute);
+router.get("/aswers/:value", aswerController.execute)
 export { router }
